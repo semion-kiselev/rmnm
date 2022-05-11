4783e9d865814482d1a3ef1ce4c943331f9c493c
Siamion Kisialiou <siamion_kisialiou@epam.com>
2022-05-11 16:43:58 +0300

    mod package.json

--- remove-node-modules-dirs.test.js
+++ remove-node-modules-dirs.test.js
@@ -2,7 +2,7 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const { mkdirSync, rmSync, writeFileSync, readdirSync, statSync } = require('fs');
 const { resolve } = require('path');
-const removeNodeModulesDirs = require('./index');
+const removeNodeModulesDirs = require('./remove-node-modules-dirs');
 
 const createTestDirs = () => {
 /*
